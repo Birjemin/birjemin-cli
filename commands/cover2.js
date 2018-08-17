7a44409de04b7c5dce45835b9b1efc2f0e6d6d92
birjemin <birjemin@foxmail.com>
2018-08-17 23:06:25 +0800

    add

--- commands/cover2.js
+++ commands/cover2.js
@@ -1,67 +1,77 @@
 const fs = require('fs')
 const gm = require('gm')
 const request = require('request')
+const qr = require('qr-image')
 
 class Image {
 
-  constructor(coverBg, cover, title, author, num) {
+  constructor(coverBg, cover, url, title, author, num) {
     this.coverBg = coverBg
     this.cover = cover
+    this.url = url
     this.title = title
     this.author = author
     this.num = num
+    this.qr = '/data/thumb_qr.png'
+    this.head = '/data/thumb_head.png'
   }
 
   async make() {
-    this.cover = await this.getImage(this.cover)
+    await this.createHead()
+    await this.createQr()
     // start
     gm(this.coverBg)
-    .draw('image Over 10, 10, 140, 140 "' + this.cover + '"')
+    .draw('image Over 10, 10, 140, 140 "' + this.head + '"')
+    .draw('image Over 180, 180, 70, 70 "' + this.qr + '"')
     .font('./font/微软雅黑.ttf')
-    .drawText(10, 186, this.title)
+    .drawText(10, 172, this.title)
+    .fontSize(11)
+    .drawText(10, 204, this.author)
+    .drawText(10, 230, this.num.toString() + '次播放')
     .fontSize(12)
-    .drawText(10, 214, this.author)
-    .drawText(10, 240, this.num.toString() + '次播放')
-    .fontSize(18)
     .fill('#000')
     .write(this.getImagePath(), err => {
       err && console.log(err.message || '出错了！');
       // delete cover
-      fs.unlink(this.cover, err => {
+      fs.unlink(this.head, err => {
+        err && console.log(err.message || '删除失败！')
+      })
+      fs.unlink(this.qr, err => {
         err && console.log(err.message || '删除失败！')
       })
     })
     console.log('Done...（请查看data目录）')
   }
 
-  async getImage(url) {
+  async createQr() {
+    let qr_svg = qr.image(this.url, { type: 'png' });
+    qr_svg.pipe(require('fs').createWriteStream(this.qr));
+  }
+
+  async createHead() {
     let state = await new Promise((resolve) => {
-      fs.exists(url, exists => {
+      fs.exists(this.cover, exists => {
         resolve(exists ? true : false)
       })
     }).catch(err => {
       console.log('image not exist')
     });
     if (state) {
-      return url
+      return this.cover
     }
 
-    let name = this.getImagePath()
     let exsist = await new Promise(resolve => {
-      gm(request(url)).write(name, err => {
+      gm(request(this.cover)).write(this.head, err => {
         resolve(err)
       })
     })
-    if (!exsist) {
-      return name
-    } else {
-      console.log('image save fail')
-    }
+    exsist && console.log('image save fail')
   }
 
   getImagePath() {
     return './data/' + this.getImageName()
   }
+
   getImageName() {
     return Date.now() + this.getRandomInt(1000) + '.png'
   }
@@ -69,14 +79,14 @@ class Image {
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-
 }
 
 const image = new Image(
   './images/cover.png',
-  'http://p1.music.126.net/eMAnm5YCLJfCTLl5cHxAGg==/109951163460988989.jpg?param=140y140',
-  'Hello World哈哈',
-  'Heelo sdfs 哇咔咔',
-  290000
+  'http://p1.music.126.net/Cl0-NpZ0ESTDjJ1HmZ33KA==/109951163460576279.jpg?param=140y140',
+  'http://music.163.com/playlist?id=2312165875',
+  '100首华语民谣，因为懂得才有共鸣',
+  '情思天鹅',
+  240000
 )
 image.make()
